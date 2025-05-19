7ad46caf1f8abb9bd79e95c79a625bc90b553f03
TrungNghiaX <104517120+nnghiax@users.noreply.github.com>
2025-05-19 15:03:28 +0700

    add model product

---
+++ server/model/Product.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    storeId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "stores",
+      required: true,
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "categories",
+      required: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    image: String,
+    description: {
+      type: String,
+      default: "",
+    },
+    sizes: {
+      type: [String],
+      enum: ["XS", "S", "M", "L", "XL", "XXL"],
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("products", productSchema);
