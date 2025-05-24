31855d2aaceca3073f1fd0a86a41443646429f7b
Trung Nguyen <nguyenpthe172112@fpt.edu.vn>
2025-05-24 15:47:28 +0700

    Front-end Login,Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-
+import LoginPage from "./components/auth/LoginPage";
+import RegisterPage from "./components/auth/RegisterPage";
+import ForgetPasswordPage from "./components/auth/ForgetPasswordPage";
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forget-password" element={<ForgetPasswordPage />} />
+      </Routes>
+    </Router>
   );
 }
 
