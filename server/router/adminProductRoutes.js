6fc59b626799019aad011690af6aba8a95adce4e
kien23102003 <kiendthe170738@fpt.edu.vn>
2025-06-07 23:58:23 +0700

    updateCode

--- server/router/adminProductRoutes.js
+++ server/router/adminProductRoutes.js
@@ -4,5 +4,7 @@ const adminProductController = require('../controller/adminProductController');
 const { verifyToken, verifyAdmin } = require('../controller/middleware');
 
 router.patch('/products/:productId/visibility', verifyToken, verifyAdmin, adminProductController.toggleProductVisibility);
+router.get('/products/low-rated', verifyToken, verifyAdmin, adminProductController.getLowRatedProducts);
+router.get('/products/test', adminProductController.testGetReviews);
 
 module.exports = router;
