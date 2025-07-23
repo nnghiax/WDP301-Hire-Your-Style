a1cabb08b2045ff1b675253dfdf9b65df5179709
nguyenhuy1807 <huyng1807@gmail.com>
2025-07-23 21:11:40 +0700

    fix page

---
+++ client/src/pages/admin/AdminStoreRequestPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import AdminSidebar from "../../components/admin/AdminSidebar";
+import AdminStoreRequest from "../../components/admin/AdminStoreRequest";
+function AdminDashboardPage() {
+  return (
+    <div>
+      <AdminSidebar />
+      <AdminStoreRequest />
+    </div>
+  );
+}
+
+export default AdminDashboardPage;
